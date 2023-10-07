1a34f6266144682128f728ab0be678862493f6e6
Sruthi Sivasankar <113873706+SruthiSivasankar@users.noreply.github.com>
2023-10-06 20:14:23 -0400

    intest (#5)

--- tests/healthz.test.js
+++ tests/healthz.test.js
@@ -4,7 +4,7 @@ const app = require('../app'); // Import your Express app
 const { expect } = chai;
 
 chai.use(chaiHttp);
-
+//testing with branch protection
 describe('Integration Test for /healthz endpoint', () => {
   it('should return status 200 and "OK"', async () => {
     const res = await chai.request(app).get('/healthz');
