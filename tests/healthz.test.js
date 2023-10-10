82deff8ebe619be4a77c6469a147dd4d6f89f09a
SruthiSivasankar <sivasankar.s@northeastern.edu>
2023-10-10 17:38:27 -0400

    demo

--- tests/healthz.test.js
+++ tests/healthz.test.js
@@ -7,7 +7,7 @@ chai.use(chaiHttp);
 //testing with branch protection
 describe('Integration Test for /healthz endpoint', () => {
   it('should return status 200 and "OK"', async () => {
-    const res = await chai.request(app).get('/healthz');
+    const res = await chai.request(app).get('/healthzz');
     expect(res).to.have.status(200);
   });
 });
